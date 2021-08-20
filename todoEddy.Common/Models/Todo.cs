a60e405b320ef209a1cf332cb02904453e35c72e
eddyosorio <eddyosorio244665@correo.itm.edu.co>
2021-08-19 21:12:49 -0500

    add entity

---
+++ todoEddy.Common/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace todoEddy.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
