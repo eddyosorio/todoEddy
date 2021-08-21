d2424f14c425dbfa992e004dbb0c269e5283fee9
eddyosorio <eddyosorio244665@correo.itm.edu.co>
2021-08-21 17:47:38 -0500

    Schedule

---
+++ todoEddy.Tests/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoEddy.Tests.Helpers
+{
+    public enum  LoggerTypes
+    {
+        Null,
+        List
+    }
+}
---
+++ todoEddy.Tests/Helpers/NullScope.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace todoEddy.Tests.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+        public void Dispose()
+        {
+
+        }
+
+        private  NullScope(){ }
+    }
+}
